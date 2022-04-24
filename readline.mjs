ed7f8e567f269026f35d0b62bbe6157f1de2de2e
saufiroja <saufiroja08@gmail.com>
2022-04-24 22:47:37 +0800

    📄docs: readline

---
+++ readline.mjs
@@ -0,0 +1,12 @@
+import readline from 'readline';
+import process from 'process';
+
+const input = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+input.question('Siapa nama anda??', (name) => {
+  console.log(`Hello ${name}`);
+  input.close();
+});
